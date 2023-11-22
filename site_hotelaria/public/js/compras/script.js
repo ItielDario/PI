d1626249ac950448c33aa18d6858d5381bf6523c
Itiel Dario <itiel.dario@gmail.com>
2023-11-21 21:35:10 -0300

    Criando botão de excluir na lista (nao esta funcionando 100%)

--- site_hotelaria/public/js/compras/script.js
+++ site_hotelaria/public/js/compras/script.js
@@ -1,6 +1,15 @@
 const btnAdicionar = document.querySelector('#btn-adicionar');
+const btnGravar = document.querySelector('#btn-gravar');
+
+btnGravar.addEventListener('click', gravarProdutos);
+
+function gravarProdutos(){
+    console.log('aaaaaaaaaa')
+}
+
 let listaProdutos = [];
 let valorTotalCompra = 0;
+let idProduto = 0;
 
 btnAdicionar.addEventListener('click', adicionarProduto);
 
@@ -10,11 +19,8 @@ function adicionarProduto(){
     const produto = document.querySelector('#produto');
     const quantidade = document.querySelector('#quantidade');
     const valorUni = document.querySelector('#valor-uni');
-    const tabelaProdutos = document.querySelector('#lista-produtos');
+    
     const valorTotalProduto = document.querySelector('.valor-total-produtos');
-    
-    
-    tabelaProdutos.innerHTML = '';
     alertProdutos.innerHTML = '';
 
     if(validarCampos(produto.value, quantidade.value, valorUni.value)){
@@ -28,26 +34,17 @@ function adicionarProduto(){
 
         let totalProduto = Number((valorUni.value * quantidade.value));
         valorTotalCompra = (valorTotalCompra + totalProduto);
-        
 
         listaProdutos.push({
+            id: `${idProduto}`,
             produto: `${produto.value}`,
             quantidade: `${quantidade.value}`,
             valorUnitario: (valorUni.value * 1).toFixed(2),
             valorTotalProduto: totalProduto.toFixed(2)
         });
 
-        for(let i = 0; i < listaProdutos.length; i++){
-            tabelaProdutos.innerHTML += 
-            `
-                <td>${i}</td>
-                <td>${listaProdutos[i].produto}</td>
-                <td>${listaProdutos[i].quantidade}</td>
-                <td>R$ ${listaProdutos[i].valorUnitario}</td>
-                <td>R$ ${listaProdutos[i].valorTotalProduto}</td>
-                <td><a class="btn-excluir"><img id="${i}" src="/img/btn_excluir.png" alt="Botão de exluir"></a></td>
-            `
-        }
+        
+        exibirTabela(listaProdutos);
 
         valorTotalProduto.innerHTML = `<h3>Valor total: R$ ${valorTotalCompra.toFixed(2)} </h3>`;
         produto.value = '';
@@ -61,15 +58,12 @@ function adicionarProduto(){
             valorTotalProduto.innerHTML = `<h3>Valor total: R$ ${valorComDesconto.toFixed(2)} </h3>`;
         })
 
-        const boteosExcluir = document.querySelector('.btn-excluir');
-        console.log(boteosExcluir)
-        for(let i = 0; i < boteosExcluir.length; i++){
-            boteosExcluir[i].addEventListener('clcik', (evt) => {
-                console.log(evt)
-            });
+        const btn = document.querySelectorAll(`.btn-excluir`);
+        for(let i = 0; i < btn.length; i++){
+            btn[i].addEventListener('click', excluir);
         }
-        
-       
+
+        idProduto++;
     }
     else{
         setTimeout(() => {
@@ -87,6 +81,43 @@ function validarCampos(produto, quantidade, valorUni){
     }
 }
 
-function excluir(evt){
-    console.log(evt.target.id)
+function exibirTabela(listaProdutos){
+    const tabelaProdutos = document.querySelector('#lista-produtos');
+    tabelaProdutos.innerHTML = '';
+    
+    for(let i = 0; i < listaProdutos.length; i++){
+        tabelaProdutos.innerHTML += 
+        `
+            <tr>
+                <td>${listaProdutos[i].id}</td>
+                <td>${listaProdutos[i].produto}</td>
+                <td>${listaProdutos[i].quantidade}</td>
+                <td>R$ ${listaProdutos[i].valorUnitario}</td>
+                <td>R$ ${listaProdutos[i].valorTotalProduto}</td>
+                <td><a class="btn-excluir"><img id="e${listaProdutos[i].id}" src="/img/btn_excluir.png" alt="Botão de exluir"></a></td>
+            </tr>
+        `
+    }
 }
+
+function excluir(evt){
+    let str = evt.target.id;
+    const idProdutoEvt = str.replace(/[^0-9]/g, '');
+
+    let vetAux = [];
+    for(let i = 0; i < listaProdutos.length; i++){
+        console.log(listaProdutos[i].id)
+        console.log(idProdutoEvt)
+        if(listaProdutos[i].id != idProdutoEvt){
+            console.log('salva');
+            vetAux.push(listaProdutos[i]);
+        }
+        else{
+            console.log('NAO salva');
+        }
+    }
+
+    listaProdutos = vetAux;
+    exibirTabela(listaProdutos);
+}
+
