1caf036f336e8648651998ea35be32bc63e24f9d
Gustavo Silva Souza <gubivistar58@hotmail.com>
2023-11-25 13:17:40 -0300

    1- adicao do logout, 2- validador de cnpj, 3- Fiscal sem acento no i, 4- adicao do botao de retornar a pagina do hotel no login, 5- adicao de verificacao se o usuario realmente deseja excluir um fornecedor

--- site_hotelaria/public/js/fornecedores/excluir.js
+++ site_hotelaria/public/js/fornecedores/excluir.js
@@ -4,27 +4,29 @@ const alertMsg = document.querySelector('#alert-msg');
 for (let i = 0; i < btnExclusao.length; i++){
     btnExclusao[i].addEventListener('click', (evt) => {
 
-        const cnpjSelecionado = {
-            cnpj: evt.target.id
-        };
+        if(confirm('Tem certeza que deseja excluir?')){
+            const cnpjSelecionado = {
+                cnpj: evt.target.id
+            };
 
-        fetch('/adm/fornecedor', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(cnpjSelecionado),
-        })
-        .then((resposta1) => {
-            return resposta1.json();
-        })
-        .then((resposta2) => {
-            if(resposta2.ok){
-                window.location.reload();
-            }
-            else{
-                alert(resposta2.msg)
-            }
-        })
+            fetch('/adm/fornecedor', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(cnpjSelecionado),
+            })
+            .then((resposta1) => {
+                return resposta1.json();
+            })
+            .then((resposta2) => {
+                if(resposta2.ok){
+                    window.location.reload();
+                }
+                else{
+                    alert(resposta2.msg)
+                }
+            })
+        }    
     }); 
 }
