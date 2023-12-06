7f7a896f87617a1279a97b71c8492fd1eb9b52c5
Vlademir Bergamini <116750634+VBergamini@users.noreply.github.com>
2023-12-06 17:06:29 -0300

    Adicionado feedback no erro do login

--- site_hotelaria/public/js/login/index.js
+++ site_hotelaria/public/js/login/index.js
@@ -27,7 +27,10 @@ document.addEventListener('DOMContentLoaded', function() {
                     window.location.href = "/adm";
                 }
                 else{
-                    document.getElementById("msgRetorno").innerHTML = '<div class="alert alert-danger">'+ r.msg +'</div>';
+                    document.getElementById("msgRetorno").innerHTML = '<p style="color: #ff0000;background-color: #f8d7da; border-color: #d6e9c6;padding: 15px; margin-bottom: 20px; border: 1px solid transparent; border-radius: 4px;">'+ r.msg +'</p>';
+                    setTimeout(() => {
+                        document.getElementById("msgRetorno").innerHTML = '';
+                      }, 4000);
                 }
             })
 
